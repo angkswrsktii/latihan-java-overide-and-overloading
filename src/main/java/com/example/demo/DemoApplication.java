1f71047145474931f38ee4b64b2cffb0e42da345
angkswrskti22 <angkasawari.sakti@salt.co.id>
2024-02-14 05:54:54 +0700

    feat: [LOEN-1234] [Sakti] Belajar override and over loading

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,28 +9,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-		OverrideMain overrideMain = new OverrideMain();
-		overrideMain.main(args);
-	}
+		Mobil mobil = new Mobil();
+		Kendaraan kendaraan = new Kendaraan();
+		mobil.jalan();
+		kendaraan.jalan();
 
-	public static class OverrideMain {
-		public void main(String args[]) {
-			Mobil mobil = new Mobil();
+		Matematika matematika1 = new Matematika();
 
-			Kendaraan kendaraan = new Kendaraan();
-			mobil.jalan();
-			kendaraan.jalan();
+		int result1 = matematika1.perkalian1(2, 3);
+		System.out.println("Result dua integer value: " + result1);
 
-			Matematika matematika1 = new Matematika();
+		int result2 = matematika1.perkalian2(2, 3, 4);
+		System.out.println("Result tiga integer value: " + result2);
 
-			int result1 = matematika1.perkalian1(2, 3);
-			System.out.println("Result dua integer value: " + result1);
-
-			int result2 = matematika1.perkalian2(2, 3, 4);
-			System.out.println("Result tiga integer value: " + result2);
-
-			matematika1.hasil(true, "Halo", 5);
-		}
+		matematika1.hasil(true, "Halo", 5);
 	}
 }
